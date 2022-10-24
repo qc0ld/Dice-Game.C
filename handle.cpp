a5c7c17c5169f91fcda2338274ac6f74382634a6
qc0ld <then7w@yandex.ru>
2022-10-24 15:00:28 +0300

    исправил перегрузку операторов и доделал тесты

--- handle.cpp
+++ handle.cpp
@@ -21,7 +21,7 @@ int read_menu_int(const char *name) {
     int number;
     std::cout << "Enter " << name << ": ";
     cin >> number;
-    while (cin.fail() || (number < 0 || number > 12)) {
+    while (cin.fail() || (number < 0 || number > 13)) {
         cin.clear();
         cin.ignore(1024, '\n');
         cout << "Enter correct number: " << endl;
